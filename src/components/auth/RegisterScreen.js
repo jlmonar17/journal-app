bfcf09f0c114e3def39beaa7341cf14c7689d8db
José Luis <jl_mj_17@hotmail.com>
2021-07-22 16:06:39 -0500

    Added styles for AuthRouter, LoginScreen and RegisterScreen.

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,7 +1,51 @@
+import { Link } from "react-router-dom";
+
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>RegisterScreen</h1>
-        </div>
+        <>
+            <h3 className="auth__title">Register</h3>
+
+            <form>
+                <input
+                    className="auth__input"
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    autoComplete="off"
+                />
+                <input
+                    className="auth__input"
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    autoComplete="off"
+                />
+                <input
+                    className="auth__input"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    autoComplete="off"
+                />
+                <input
+                    className="auth__input"
+                    type="password"
+                    placeholder="Confirm password"
+                    name="password2"
+                    autoComplete="off"
+                />
+
+                <button
+                    type="submit"
+                    className="btn btn-primary btn-block mb-5"
+                >
+                    Login
+                </button>
+
+                <Link to="/auth/login" className="link">
+                    Alredy registered?
+                </Link>
+            </form>
+        </>
     );
 };
