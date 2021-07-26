3c527cadf049100b7ed0c4d3fbfb002ffbc1b1a0
José Luis <jl_mj_17@hotmail.com>
2021-07-26 03:06:36 -0500

    Keeping authentication state after refreshing page.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import {
     BrowserRouter as Router,
     Redirect,
@@ -6,8 +8,23 @@ import {
 } from "react-router-dom";
 import { JournalScreen } from "../components/journal/JournalScreen";
 import { AuthRouter } from "./AuthRouter";
+import { firebase } from "../firebase/firebase-config";
+import { login } from "../actions/auth";
 
 export const AppRouter = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        // It creates an observable, it fires when authentication change, when user
+        // authenticates again, when user login without refresh page, etc
+        firebase.auth().onAuthStateChanged((user) => {
+            // If user exist, that means that user is authenticated.
+            if (user?.uid) {
+                dispatch(login(user.uid, user.email));
+            }
+        });
+    }, [dispatch]);
+
     return (
         <Router>
             <div>
