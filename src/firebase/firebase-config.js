f58e9dce5b921f0ba86b5af060507a0e2028e728
José Luis <jl_mj_17@hotmail.com>
2021-08-02 01:04:27 -0500

    Added environment variables for testing and development environments.

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,29 +3,15 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBI2Ek-hfIfF3szORyl1p7BgpM3Vc68FsQ",
-    authDomain: "curso-react-cero-exp.firebaseapp.com",
-    projectId: "curso-react-cero-exp",
-    storageBucket: "curso-react-cero-exp.appspot.com",
-    messagingSenderId: "803361469520",
-    appId: "1:803361469520:web:b41f625b51a20bfc8a0d55",
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
 };
 
-const firebaseConfigTest = {
-    apiKey: "AIzaSyBmej95enrFXMnAHIPlF7ylNUgQEiC7dMA",
-    authDomain: "react-journal-testing-jm.firebaseapp.com",
-    projectId: "react-journal-testing-jm",
-    storageBucket: "react-journal-testing-jm.appspot.com",
-    messagingSenderId: "433899802856",
-    appId: "1:433899802856:web:77149cab5d44a3f284f95d",
-};
-
-// Initialize Firebase acording to environment
-if (process.env.NODE_ENV === "test") {
-    firebase.initializeApp(firebaseConfigTest);
-} else {
-    firebase.initializeApp(firebaseConfig);
-}
+firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
