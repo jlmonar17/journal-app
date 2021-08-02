5be39f8c8b784219ec641183d559a6988602505c
José Luis <jl_mj_17@hotmail.com>
2021-08-02 00:18:17 -0500

    Asynchronus tests for startNewNote. Using redux-mock-store.

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -2,7 +2,6 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBI2Ek-hfIfF3szORyl1p7BgpM3Vc68FsQ",
     authDomain: "curso-react-cero-exp.firebaseapp.com",
@@ -11,8 +10,22 @@ const firebaseConfig = {
     messagingSenderId: "803361469520",
     appId: "1:803361469520:web:b41f625b51a20bfc8a0d55",
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+const firebaseConfigTest = {
+    apiKey: "AIzaSyBmej95enrFXMnAHIPlF7ylNUgQEiC7dMA",
+    authDomain: "react-journal-testing-jm.firebaseapp.com",
+    projectId: "react-journal-testing-jm",
+    storageBucket: "react-journal-testing-jm.appspot.com",
+    messagingSenderId: "433899802856",
+    appId: "1:433899802856:web:77149cab5d44a3f284f95d",
+};
+
+// Initialize Firebase acording to environment
+if (process.env.NODE_ENV === "test") {
+    firebase.initializeApp(firebaseConfigTest);
+} else {
+    firebase.initializeApp(firebaseConfig);
+}
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
